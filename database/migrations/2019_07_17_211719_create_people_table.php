29d81f4292218c63e89133aa1f387c7dd097ff9c
Deniss Celuiko <deniss.celuiko@gmail.com>
2019-07-18 12:03:03 +0300

    Partial model creation

---
+++ database/migrations/2019_07_17_211719_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('phone')->nullable();
+            $table->string('email-personal')->nullable();
+            $table->string('email-work')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('pin')->nullable()->comment('Latvian personal identification number');
+            $table->string('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
